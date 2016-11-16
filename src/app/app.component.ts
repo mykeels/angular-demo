3ee1ac88b87de21f8f20ee2882862f6df0a305fa
Michael Ikechi <mykehell123@gmail.com>
2016-11-16 14:26:02 +0100

    learned to use services and providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
 export class AppComponent {
+  constructor(private heroService: HeroService) {}
+  ngOnInit(): void {
+    this.heroes = this.getHeroes();
+  }
   title = 'Tour of Heroes!';
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
-  heroes: Hero[] = [
-    {
-      id: 11, name: 'Mr. Nice'
-    },
-    {
-      id: 12, name: 'Mr. Wong'
-    },
-    {
-      id: 13, name: 'Mr. Song'
-    },
-    {
-      id: 14, name: 'Ms. Beets'
-    },
-    {
-      id: 15, name: 'Mr. Sharknado'
-    }
-  ];
+  getHeroes(): Hero[] {
+    return this.heroService.getHeroes();
+  }
+  heroes: Hero[];
 }
