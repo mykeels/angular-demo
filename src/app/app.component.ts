fb8a369c9a91e8e397f41f2bca4b50d8c162cb5e
Michael Ikechi <mykehell123@gmail.com>
2016-11-16 13:20:10 +0100

    created angular-demo, for angular 2 practise.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes!';
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  };
+  heroes: Hero[] = [
+    {
+      id: 11, name: 'Mr. Nice'
+    },
+    {
+      id: 12, name: 'Mr. Wong'
+    },
+    {
+      id: 13, name: 'Mr. Song'
+    },
+    {
+      id: 14, name: 'Ms. Beets'
+    },
+    {
+      id: 15, name: 'Mr. Sharknado'
+    }
+  ];
 }
